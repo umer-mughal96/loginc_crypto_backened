289162ea0e16e043b33c20e3d725a36f7179fab4
asim <asim92578@gmail.com>
2022-01-03 10:28:00 +0500

    new changes deploy

--- cronSchedule/cronSchedule.js
+++ cronSchedule/cronSchedule.js
@@ -1,23 +1,23 @@
+// const cron          =   require('node-cron');
+// const cronBinance   =   require('../crons/binance');
 
-const cron          =   require('node-cron');
-const cronBinance   =   require('../crons/binance');
-
-//run after every 2 sec
-cron.schedule('*/4 * * * * *', () => {
+// //run after every 2 sec
+// cron.schedule('*/4 * * * * *', () => {
         
-    // cronBinance.price();
-});
+//     cronBinance.price();
+// });
 
-cron.schedule('0 0 */1 * * *', () => {
-    // cron.schedule('*/4 * * * * *', () => {
+// // cron.schedule('0 0 */1 * * *', () => {
+//     cron.schedule('*/10 * * * * *', () => {
     
-    // cronBinance.balanceUpdate();
-});
+//     cronBinance.balanceUpdate();
+// });
 
 
-cron.schedule('*/5 * * * * *' , () => {
+// cron.schedule('*/5 * * * * *' , () => {
 
-    cronBinance.volume();
-})
+//     cronBinance.volume();
+// })
+
 
 
